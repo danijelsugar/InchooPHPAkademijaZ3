a514b15752d18bebb70bb79bbdcb2c9a6375a1e5
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-23 14:15:30 +0100

    Starting w3c valid template

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
 
+    <title>Homework 3</title>
+    <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
